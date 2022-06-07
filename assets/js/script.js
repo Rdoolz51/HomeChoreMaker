6f5d0f515c2a7013c23cea83097b5e66e64d1637
Ryan Dooley <Rdoolz51@aol.com>
2022-06-07 10:26:30 -0400

    add assignee value to each task

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,23 @@ formEl.addEventListener("submit", createTaskHandler);
 function createTaskHandler(event) {
 
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskAsigneeInput = document.querySelector("select[name='task-assignee']").value;
     
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task!";
+    
+    //create Div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-assignee'>" + taskAsigneeInput + "</span>";
+
+    //add the div to the list item
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
