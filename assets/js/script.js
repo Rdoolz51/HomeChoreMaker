f36fb75d39e111ef87ddcbf9546a27ce7440c5ca
Ryan Dooley <Rdoolz51@aol.com>
2022-06-07 12:22:33 -0400

    refactored functions in script.js to be more re-usable/readable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
-function createTaskHandler(event) {
+function taskFormHandler(event) {
 
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskAsigneeInput = document.querySelector("select[name='task-assignee']").value;
+
+    var taskDataObj = {
+        name: taskNameInput,
+        assignee: taskAsigneeInput
+    };
+
+    createTaskEl(taskDataObj);
     
-    //create list item
+}
+
+function createTaskEl(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     
     //create Div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-assignee'>" + taskAsigneeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-assignee'>" + taskDataObj.assignee + "</span>";
 
     //add the div to the list item
     listItemEl.appendChild(taskInfoEl);
